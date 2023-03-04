f4d541c86f7ee5ed3c418d24ffc0acf5de988b1d
luism <luism@192.168.56.1>
2023-03-04 00:05:20 -0500

    Instant can now be parsed between TimeStamp. Made for JPA

---
+++ src/main/java/edu/epn/web/b2022/g6/appweb/chauchera/models/daos/runtime/JPAInstantAttributeConverter.java
@@ -0,0 +1,20 @@
+package edu.epn.web.b2022.g6.appweb.chauchera.models.daos.runtime;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Converter(autoApply = true)
+public class JPAInstantAttributeConverter implements AttributeConverter<Instant, Timestamp> {
+
+    @Override
+    public Timestamp convertToDatabaseColumn(Instant instant) {
+        return instant == null ? null : Timestamp.from(instant);
+    }
+
+    @Override
+    public Instant convertToEntityAttribute(Timestamp timestamp) {
+        return timestamp == null ? null : timestamp.toInstant();
+    }
+}

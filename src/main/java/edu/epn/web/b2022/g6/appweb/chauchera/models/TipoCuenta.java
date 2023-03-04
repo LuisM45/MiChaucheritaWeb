757a0ec81cb75a87b4447fab1e88459f4cae07bf
luism <luism@192.168.56.1>
2023-03-04 13:42:50 -0500

    Implemented equals() and hash()

--- src/main/java/edu/epn/web/b2022/g6/appweb/chauchera/models/TipoCuenta.java
+++ src/main/java/edu/epn/web/b2022/g6/appweb/chauchera/models/TipoCuenta.java
@@ -1,19 +1,21 @@
 package edu.epn.web.b2022.g6.appweb.chauchera.models;
 
 import edu.epn.web.b2022.g6.appweb.chauchera.models.daos.TipoCuentaDAO;
+import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
 
 @Entity
 @Table(name="account_type")
-public class TipoCuenta {
+public class TipoCuenta implements Serializable {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -54,6 +56,30 @@ public class TipoCuenta {
     @Override
     public String toString() {
         return nombre;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TipoCuenta other = (TipoCuenta) obj;
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        return Objects.equals(this.id, other.id);
     }
     
     
